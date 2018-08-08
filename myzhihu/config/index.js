d5818c752e82ff68452f07aa1aad8dbca1f0db19
lianming <1097364388@qq.com>
2018-08-08 14:39:47 +0800

    vue开发环境跨域test1

--- myzhihu/config/index.js
+++ myzhihu/config/index.js
@@ -45,7 +45,22 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: true,
+
+
+    // 增加跨域支持
+    proxyTable: {
+      '/api': {
+        // target: 'http://wx.4006188666.com/api/', //设置调用接口域名和端口号别忘了加http
+        target: 'http://news-at.zhihu.com/api/',  // 知乎日报
+        // target: 'http://zhihuapi.herokuapp.com/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/' //这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用api代替
+          // 比如我要调用'http://0.0:300/user/add'，直接写‘/api/user/add’即可
+        }
+      }
+    },
   },
 
   build: {
