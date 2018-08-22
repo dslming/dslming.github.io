0ce1b54fe9325b78d9842f7d42f7ba9af4c5b9d4
lianming <lianming@meishubao.com>
2018-08-22 15:10:15 +0800

    first

---
+++ wokegongyu-vue/wokegongyu/src/components/head.vue
@@ -0,0 +1,57 @@
+<template>
+     <header class="woke_header">
+          <div class="logo">蜗壳公寓</div>
+            <div class="city">
+                <span class="fa fa-map-marker"></span>
+                <p>青岛</p>
+                <span class="fa fa-angle-down"></span>
+            </div>
+     </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.woke_header{
+    height: 40px;
+    background-color: $blueColor;
+    align-items: center;
+    padding-right: 15px;
+    padding-left: 15px;
+    display: flex;
+    justify-content: space-between;
+
+    .logo{
+        font-size: 18px;
+        line-height: 18px;
+        color:#fff;
+        font-weight: 800;
+    }
+
+    .city{
+        width: 25%;;
+        height: 55%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #fff;
+        font-size:12px;
+        border-radius: 15px;
+        color:$blueColor;
+        padding:0 5px;
+
+        p{
+            color:$blueColor;
+            padding:0;
+            margin:0;
+        }
+
+        span{
+           
+            font-size: 14px;
+        }
+    }
+}
+</style>
---
+++ wokegongyu-vue/wokegongyu/src/page/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <head></head>
+    </div>
+</template>
+
+<script>
+import head from '@/components/head'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
