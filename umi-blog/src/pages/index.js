36e9ea539f431fa3f03e50011350588f2d93c7e7
lm13069974838 <1097364388@qq.com>
2019-07-01 18:03:20 +0800

    修改项目主页目录

--- umi-blog/src/pages/index.js
+++ umi-blog/src/pages/index.js
@@ -1,5 +1,3 @@
-import Header from './components/header'
-import Main from './components/Main'
 import Experience from './components/Experience'
 import Education from './components/Education'
 import styles from './index.less';
@@ -7,8 +5,6 @@ import styles from './index.less';
 export default function() {
   return (
     <div className={styles.normal}>
-      <Header />
-      <Main />
       <Experience />
       <Education />
     </div>
