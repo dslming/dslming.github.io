fc5d9f23bbb47c8e9e9ef1302e87a9b62915be1a
lm13069974838 <1097364388@qq.com>
2019-07-01 19:46:30 +0800

    上线

--- umi-blog/publish.js
+++ umi-blog/publish.js
@@ -17,7 +17,7 @@ function saveFile(name, end) {
                 console.log(`save ${name} succ`);  
                 if(end) {
                    setTimeout(()=>{
-                    shell.exec('git add .')
+                    shell.exec('git add -u')
                     shell.exec('git commit -m "上线"')
                     shell.exec('git push"')
                    }, 3000)
