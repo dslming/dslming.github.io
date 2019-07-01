8d018ef20117fa63736a236447f1af5b63a681b9
lm13069974838 <1097364388@qq.com>
2019-07-01 19:48:51 +0800

    上线

--- umi-blog/src/pages/components/Experience.jsx
+++ umi-blog/src/pages/components/Experience.jsx
@@ -5,7 +5,7 @@ function Experience(props) {
     <div className={style.experience}>
       <h4>项目主页</h4>
       <div className={style.enterprise}>
-        <p className={style.time_now}>2019/7/1</p>
+        <p className={style.time_now}>2019/7/1 19:48:55</p>
         <div className={style.detail}>
             <div className={style.desc}>
                 <p><a href="https://dslming.github.io/ThreejsLearning/007-ff91/index.html"> FF91 汽车</a></p>
