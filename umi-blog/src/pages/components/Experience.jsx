b1652b1dde9de78ad84b183e6726521298b77c3b
lm13069974838 <1097364388@qq.com>
2019-07-08 14:06:24 +0800

    上线

--- umi-blog/src/pages/components/Experience.jsx
+++ umi-blog/src/pages/components/Experience.jsx
@@ -5,6 +5,16 @@ function Experience(props) {
     <div className={style.experience}>
       <h4>项目主页</h4>
       <div className={style.enterprise}>
+      <p className={style.time_now}>2019/7/8 11:52</p>
+        <div className={style.detail}>
+            <div className={style.desc}>
+                <p><a href="https://dslming.github.io/mapbox/demo.html">地图2</a></p>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+
         <p className={style.time}>2019/7/8 11:52</p>
         <div className={style.detail}>
             <div className={style.desc}>
@@ -15,7 +25,7 @@ function Experience(props) {
             </div>
         </div>
 
-        <p className={style.time_now}>2019/7/1 19:48:55</p>
+        <p className={style.time}>2019/7/1 19:48:55</p>
         <div className={style.detail}>
             <div className={style.desc}>
                 <p><a href="https://dslming.github.io/ThreejsLearning/007-ff91/index.html"> FF91 汽车</a></p>
