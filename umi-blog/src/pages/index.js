c984c035dfd7161267fbd1fb965099548efe26b5
lm13069974838 <1097364388@qq.com>
2019-09-10 15:07:07 +0800

    增加背景

--- umi-blog/src/pages/index.js
+++ umi-blog/src/pages/index.js
@@ -1,12 +1,14 @@
 import Experience from './components/Experience'
 import Footer from './components/Footer'
+import Back from './components/Back'
 import styles from './index.less';
 
-export default function() {
+export default function () {
   return (
     <div className={styles.normal}>
+      <Back />
       <Experience />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
