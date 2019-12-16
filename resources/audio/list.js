ccd1376a9a00f066794f4e22802df15618e6e2ab
lm13069974838 <1097364388@qq.com>
2019-12-16 14:55:14 +0800

    增加音乐文件

---
+++ resources/audio/list.js
@@ -0,0 +1,66 @@
+let musicList = [
+  {
+    index: 0,
+    name: "1, 2, 3! (Japanese Ver.)",
+    artist: "胜利",
+    cover: "0.jpg",
+    source: "0.mp3",
+    favorited: false
+  },
+  {
+    index: 1,
+    name: "Hi Hi Bye Bye",
+    artist: "王心凌",
+    cover: "1.jpg",
+    source: "1.mp3",
+    favorited: false
+  },
+  {
+    index: 2,
+    name: "热雪",
+    artist: "魏晨",
+    cover: "1.jpg",
+    source: "1.mp3",
+    favorited: false
+  },
+  {
+    index: 3,
+    name: "人生浪费指南",
+    artist: "夏日入侵企画",
+    cover: "3.jpg",
+    source: "3.mp3",
+    favorited: false
+  },
+  {
+    index: 4,
+    name: "bad guy",
+    artist: "Billie Eilish",
+    cover: "4.jpg",
+    source: "4.mp3",
+    favorited: false
+  },
+  {
+    index: 5,
+    name: "2002",
+    artist: "J.Fla",
+    cover: "5.jpg",
+    source: "5.mp3",
+    favorited: false
+  },
+  {
+    index: 6,
+    name: "Monsters",
+    artist: "Katie Sky",
+    cover: "6.jpg",
+    source: "6.mp3",
+    favorited: false
+  },
+  {
+    index: 7,
+    name: "Do It Again",
+    artist: "Madeline Juno",
+    cover: "7.jpg",
+    source: "7.mp3",
+    favorited: false
+  }
+]
